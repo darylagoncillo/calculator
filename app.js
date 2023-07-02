259926aeb1057d8944d6a0c78a6557d0b9d5e9f3
Daryl Agoncillo <daryl@Daryls-MacBook-Pro.local>
2023-07-01 17:43:29 -0700

    Created calculator GUI with HTML/CSS, and basic function logic

---
+++ app.js
@@ -0,0 +1,25 @@
+// Number, operator, another number (basic calc)
+let num1;
+let num2;
+let operand;
+
+// Operands
+let add = (num1, num2) => num1 + num2;
+let subtract = (num1, num2) => num1 - num2;
+let multiply = (num1, num2) => num1 * num2;
+let divide = (num1, num2) => num1 / num2;
+
+let operate = (num1, num2, operand) => {
+  switch (operand) {
+    case "add":
+      return add(num1, num2);
+    case "subtract":
+      return subtract(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+  }
+};
+
+
